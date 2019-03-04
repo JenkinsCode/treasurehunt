19de5ffc2f7818ecb8142d51490d3f8fcc330e21
Robert Jenkins <robertjenkins@Roberts-MBP.localdomain>
2019-03-04 11:03:08 -0800

    adds layout and styles for board and squares

---
+++ src/board.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import Square from './square'
+import './App.css'
+
+class Board extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      squares: ['?', '?', '?',
+                '?', '?', '?',
+                '?', '?', '?']
+    }
+  }
+  render() {
+    const{ squares } = this.state
+    return(
+      <div className = 'board'>
+      {squares.map((squareValue, index)=>{
+        return (<Square
+                key = {index}
+                value = {squareValue}
+                index = {index}
+          />
+        )
+      })}
+      </div>
+    )
+  }
+}
+
+export default Board
---
+++ src/square.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import './App.css'
+
+class Square extends Component {
+  render(){
+    const{ value } = this.props
+    return(
+      <div className = "square">
+      
+      {value}
+      </div>
+    )
+  }
+}
+
+export default Square
