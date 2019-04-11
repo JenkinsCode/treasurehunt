ce73b372bf210533a320cd99bd5b07cb0354f8c8
Robert Jenkins <robertjenkins@Roberts-MBP.localdomain>
2019-04-11 14:59:16 -0700

    This is treasure hunt

--- src/board.js
+++ src/board.js
@@ -6,23 +6,117 @@ class Board extends Component {
   constructor(props){
     super(props)
     this.state = {
-      squares: ['?', '?', '?',
-                '?', '?', '?',
-                '?', '?', '?']
+      squares: []
     }
   }
+
+  componentDidMount(){
+    this.setupGame()
+  }
+  setupGame = () =>{
+    const chest= Math.floor(Math.random()*8)
+
+    do{
+      var bomb = Math.floor(Math.random()*8)
+    } while (bomb === chest)
+
+    this.setState({
+      squares: ['?', '?', '?',
+                '?', '?', '?',
+                '?', '?', '?'],
+      bombIndex: bomb,
+      chestIndex: chest,
+      clickCount: 0,
+      gameIsLost: false,
+      gameIsWon: false,
+    })
+  }
+
+
+
+  squareAssignment = (index) => {
+    const{bombIndex, chestIndex, squares} = this.state
+    if(index === bombIndex){
+      return 'ROCK'
+    } else if(index === chestIndex){
+      return 'TREASURE'
+    } else {
+      return 'TREE'
+    }
+  }
+
+  gameIsActive = () => {
+    const {clickCount, gameIsWon, gameIsLost } = this.state
+    return clickCount < 2 && !gameIsWon && !gameIsLost
+  }
+
+  flipSquare = (index) => {
+    const {squares}= this.state
+    const newValue = this.squareAssignment(index)
+    squares[index] = newValue
+    this.setState({
+      squares: squares
+    })
+  }
+
+  flipAllSquares = () =>{
+    const {squares} = this.state
+    squares.forEach((square, index)=>{
+    this.flipSquare(index)
+  })
+}
+
+  handleSquareClick = (index)=>{
+   const{ squares, clickCount } = this.state
+   if(squares[index] !== '?'){
+      return
+    }
+
+    if(this.gameIsActive() && squares[index] === '?'){
+      const newValue = this.squareAssignment(index)
+      this.flipSquare(index)
+      this.setState({
+        clickCount: clickCount + 1,
+        gameIsWon: newValue === 'c',
+        gameIsLost: newValue === 'b',
+      })
+    } else if(clickCount >= 2){
+      this.setState({gameIsLost: true})
+    }
+  }
+
   render() {
-    const{ squares } = this.state
+    const{ gameIsLost, gameIsWon, squares } = this.state
     return(
-      <div className = 'board'>
-      {squares.map((squareValue, index)=>{
-        return (<Square
-                key = {index}
-                value = {squareValue}
-                index = {index}
-          />
-        )
-      })}
+      <div>
+        {gameIsLost &&
+          <h1> You lose!</h1>
+        }
+        {gameIsWon &&
+        <h1>You Win!</h1>
+        }
+        <div className = 'board'>
+        {squares.map((squareValue, index)=>{
+          return (<Square
+                  key = {index}
+                  value = {squareValue}
+                  index = {index}
+                  onClick = {this.handleSquareClick}
+            />
+          )
+        })}
+        </div>
+        <button
+          onClick={this.flipAllSquares}
+          >
+          give up?
+          </button>
+
+        <button
+          onClick={this.setupGame}
+          >
+          reset the game!
+          </button>
       </div>
     )
   }
--- src/square.js
+++ src/square.js
@@ -2,12 +2,20 @@ import React, {Component} from 'react'
 import './App.css'
 
 class Square extends Component {
+
+  localHandleClick = () => {
+    const{ index, onClick } = this.props
+    console.log("Clicked", index)
+    onClick(index)
+  }
+
   render(){
     const{ value } = this.props
     return(
-      <div className = "square">
-      
-      {value}
+      <div
+      onClick = {this.localHandleClick}
+      className = "square">
+        {value}
       </div>
     )
   }
